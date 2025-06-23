c95a8f05f458795c9ea2882fd877a97a1fb0286e
milekboy <tijaniwahab25@gmail.com>
2025-06-23 15:53:10 +0100

    enabled filter by price, color and size functonality on PLP page

--- src/components/HeaderSidbar.tsx
+++ src/components/HeaderSidbar.tsx
@@ -1,11 +1,32 @@
+import { useState } from "react";
 import Link from "next/link";
 import ShopSidebar from "../elements/Shop/ShopSidebar";
 
-export default function HeaderSidbar(){
-    return(
-        <div className="product-description">          
-            <ShopSidebar />
-            <Link href="#" className="btn btn-sm font-14 btn-secondary btn-sharp">RESET</Link>
-        </div>
-    )
+export default function HeaderSidbar() {
+  const [selectedColor, setSelectedColor] = useState<string | null>(null);
+  const [selectedSize, setSelectedSize] = useState<number | null>(null);
+  const [selectedPriceRange, setSelectedPriceRange] = useState<
+    [number, number]
+  >([0, 400]);
+
+  const handlePriceChange = (range: [number, number]) =>
+    setSelectedPriceRange(range);
+  const handleColorChange = (color: string) => setSelectedColor(color);
+  const handleSizeChange = (size: string) => setSelectedSize(Number(size));
+
+  return (
+    <div className="product-description">
+      <ShopSidebar
+        onPriceChange={handlePriceChange}
+        onColorChange={handleColorChange}
+        onSizeChange={handleSizeChange}
+        selectedColor={selectedColor}
+        selectedSize={selectedSize}
+        selectedPriceRange={selectedPriceRange}
+      />
+      <Link href="#" className="btn btn-sm font-14 btn-secondary btn-sharp">
+        RESET
+      </Link>
+    </div>
+  );
 }
