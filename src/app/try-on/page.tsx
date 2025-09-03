a3551e48f713f023ae24ea5e074ba23daf2c8f4d
milekboy <tijaniwahab25@gmail.com>
2025-09-03 23:22:38 +0100

    glasses overlay

--- src/app/try-on/page.tsx
+++ src/app/try-on/page.tsx
@@ -1,4 +1,9 @@
-import TryOn from "@/components/TryOn";
+"use client";
+
+import dynamic from "next/dynamic";
+
+const TryOn = dynamic(() => import("@/components/TryOn"), { ssr: false });
+
 import CommanLayout from "@/components/CommanLayout";
 import TryOn3D from "@/components/TryOn3D";
 export default function Augument() {
