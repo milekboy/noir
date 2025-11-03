3d0738557a1d4ee8276931c3c3f268ca25012caf
milekboy <tijaniwahab25@gmail.com>
2025-11-03 20:17:35 +0100

    try on

---
+++ src/app/try-on/page.jsx
@@ -0,0 +1,10 @@
+import TryOn from "@/components/TryOn";
+import CommanLayout from "@/components/CommanLayout";
+// import TryOn3D from "@/components/TryOn3D";
+export default function Augument() {
+  return (
+    <CommanLayout>
+      <TryOn />
+    </CommanLayout>
+  );
+}
