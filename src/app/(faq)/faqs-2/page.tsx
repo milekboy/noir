53cb35206f830fdf5f25369ed37314cad6a038f6
milekboy <tijaniwahab25@gmail.com>
2025-11-06 17:44:12 +0100

    suspense

--- src/app/(faq)/faqs-2/page.tsx
+++ src/app/(faq)/faqs-2/page.tsx
@@ -1,12 +1,17 @@
-"use cliengit "
+"use client";
+
+import { Suspense } from "react";
 import MainLayout from "@/components/MainLayout";
 import Faq2 from "./_components/Faq2";
 
-const Faq2Page = () =>{
-    return(
-        <MainLayout>
-            <Faq2 />
-        </MainLayout>
-    )
-}
-export default Faq2Page
+const Faq2Page = () => {
+  return (
+    <MainLayout>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Faq2 />
+      </Suspense>
+    </MainLayout>
+  );
+};
+
+export default Faq2Page;
